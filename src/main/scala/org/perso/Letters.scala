b06670a9530ef484c36f02d8fd3e15fe901f3d97
Timothée AUPETIT <timothee.aupetit@proton.com>
2024-10-10 07:53:45 +0200

    implement isAnagram method

--- src/main/scala/org/perso/Letters.scala
+++ src/main/scala/org/perso/Letters.scala
@@ -5,6 +5,13 @@ object Letters {
 
   private val alphabet = "abcdefghijklmnopqrstuvwxyz"
 
+  def isAnagram(word1: String, word2: String): Boolean = {
+    val letters1 = word1.toLowerCase
+    val letters2 = word2.toLowerCase
+
+    letters1.forall(letters2.contains) && letters2.forall(letters1.contains)
+  }
+
   def isPangram(sentence: String): Boolean = {
     val letters = sentence.toLowerCase()
 
--- src/test/scala/org/perso/LettersTest.scala
+++ src/test/scala/org/perso/LettersTest.scala
@@ -3,6 +3,24 @@ package org.perso
 import org.scalatest.funspec.AnyFunSpec
 
 class LettersTest extends AnyFunSpec {
+  describe("isAnagram") {
+    it("should return true when a word contains all the letters of the other word") {
+      val word1 = "tsar"
+      val word2 = "star"
+
+      assert(Letters.isAnagram(word1, word2))
+      assert(Letters.isAnagram(word2, word1))
+    }
+
+    it("should return false when a word does not contain all the letters of the other word") {
+      val word1 = "scar"
+      val word2 = "car"
+
+      assert(!Letters.isAnagram(word1, word2))
+      assert(!Letters.isAnagram(word2, word1))
+    }
+  }
+
   describe("isPangram") {
     it("should return true when the sentence contains all the letters of the alphabet") {
       val sentence = "Portez ce vieux whisky au juge blond qui fume"
